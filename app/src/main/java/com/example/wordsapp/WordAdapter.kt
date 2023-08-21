2caf05e3fe73e8b66899c038eb60c993a2805473
Glenn Halls <gjhalls@gmail.com>
2023-08-21 14:29:21 +1000

    Add: implement WordListFragment

--- app/src/main/java/com/example/wordsapp/WordAdapter.kt
+++ app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -15,6 +15,7 @@
  */
 package com.example.wordsapp
 
+import WordListFragment
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -86,7 +87,7 @@ class WordAdapter(private val letterId: String, context: Context) :
 
         // Assigns a [OnClickListener] to the button contained in the [ViewHolder]
         holder.button.setOnClickListener {
-            val queryUrl: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+            val queryUrl: Uri = Uri.parse("${WordListFragment.SEARCH_PREFIX}${item}")
             val intent = Intent(Intent.ACTION_VIEW, queryUrl)
             context.startActivity(intent)
         }
